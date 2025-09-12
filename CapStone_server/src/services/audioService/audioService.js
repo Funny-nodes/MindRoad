e1cda9f7eeeccf7ada60ea0dedb225d71d2b72af
MOON_KI_UP <mgumjh6@naver.com>
2025-09-12 14:35:27 +0900

    09-12/ PDF제목 루트노드로 수정

--- CapStone_server/src/services/audioService/audioService.js
+++ CapStone_server/src/services/audioService/audioService.js
@@ -73,6 +73,10 @@ exports.processIndividualFile = async (
       isRealTime
     );
 
+    if (nodeData && nodeData.length > 0) {
+      openAIResponse.rootNode = nodeData[0];
+    }
+
     // 🔽 이 부분 추가: 키워드가 있으면 바로 노드 생성
     let addedNodes = null;
 
