7971539927b646593570d60922f64810cc800119
SUNJUN KIM <161678134+SunjunSS@users.noreply.github.com>
2026-02-12 15:47:24 +0900

    Fix import case for MouseTracking component

--- CapStone_vue/src/router.js
+++ CapStone_vue/src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import MainHome from "./components/MainHome/MainHomeSideBar.vue";
 import MindMap from "./components/MindMap/MindMap.vue";
 import WeBRTC from "./components/WebRTC/WebRTC.vue";
-import MouseTracking from "./components/WebRTC/mouseTracking.vue";
+import MouseTracking from "./components/WebRTC/MouseTracking.vue";
 import MyMap from "./components/MainHome/MyMap.vue";
 import Register from "./components/User/Register.vue";
 import Login from "./components/User/Login.vue";
